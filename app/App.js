c9327a15cd756ec07e2a0ce47b09fe89e70be46b
Cameron Banowsky <41644704+shebash@users.noreply.github.com>
2018-12-17 12:18:20 -0800

    Update App.js
    g3t r3kt

--- app/App.js
+++ app/App.js
@@ -3,23 +3,35 @@ import Player from './Player';
 
 export const TRACKS = [
   {
-    title: 'Stressed Out',
-    artist: 'Twenty One Pilots',
-    albumArtUrl: "http://36.media.tumblr.com/14e9a12cd4dca7a3c3c4fe178b607d27/tumblr_nlott6SmIh1ta3rfmo1_1280.jpg",
-    audioUrl: "http://dl.fazmusics.in/Ali/music/aban/hot%20100%20.7%20nov%202015(128)/Twenty%20One%20Pilots%20-%20Stressed%20Out.mp3",
+    title: 'R3KT',
+    artist: 'Rekt (Dubstep/DnB + Live),
+    albumArtUrl: " ",
+    audioUrl: "https://stream.rekt.network/rekt.ogg"
   },
   {
-    title: 'Love Yourself',
-    artist: 'Justin Bieber',
-    albumArtUrl: "http://arrestedmotion.com/wp-content/uploads/2015/10/JB_Purpose-digital-deluxe-album-cover_lr.jpg",
-    audioUrl: 'http://srv2.dnupload.com/Music/Album/Justin%20Bieber%20-%20Purpose%20(Deluxe%20Version)%20(320)/Justin%20Bieber%20-%20Purpose%20(Deluxe%20Version)%20128/05%20Love%20Yourself.mp3',
+    title: 'REKT',
+    artist: 'Rekt (Dubstep/DnB),
+    albumArtUrl: "",
+    audioUrl: "https://stream.rekt.network/rekt.ogg"
+  },
+  { 
+    title: 'RESURREKT',
+    artist: 'Live Set Replays',
+    albumArtUrl: "",
+    audioUrl: "https://stream.rekt.network/resurrekt.ogg"
   },
   {
-    title: 'Hotline Bling',
-    artist: 'Drake',
-    albumArtUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Drake_-_Hotline_Bling.png',
-    audioUrl: 'http://dl2.shirazsong.org/dl/music/94-10/CD%201%20-%20Best%20of%202015%20-%20Top%20Downloads/03.%20Drake%20-%20Hotline%20Bling%20.mp3',
+    title: 'REKTIFY',
+    artist: 'REKTIFY Lo-Fi',
+    albumArtUrl: '',
+    audioUrl: "https://stream.rekt.network/rektify.ogg"
   },
+  {
+    title: 'REKTORY',
+    artist: 'REKTORY (Old School Reefer Jazz)',
+    albumArtUrl: '',
+    audioUrl: "https://stream.rekt.network/rektory.ogg"
+  }
 ];
 
 export default class App extends Component {
